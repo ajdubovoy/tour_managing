af5732ea5a5a4c4f03499bf73002cf3d27143c73
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-06-06 14:46:38 +0200

    add contacts index route

--- app/javascript/react_app/components/contacts_index.jsx
+++ app/javascript/react_app/components/contacts_index.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import PageContainer from './page_container';
 import LeftBar from './left_bar';
 import RightBar from './right_bar';
+import ContactsList from '../containers/contacts_list';
 
 const ContactsIndex = (props) => {
   return (
     <PageContainer>
-      <LeftBar></LeftBar>
+      <LeftBar>
+        <ContactsList />
+      </LeftBar>
       <RightBar></RightBar>
     </PageContainer>
   );
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :contacts, only: [ :index ]
+    end
+  end
+
   get '/contacts', to: 'pages#react'
   get '/tours' ,to: 'pages#react'
   get '/venues', to: 'pages#react'
   root to: 'pages#react'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
